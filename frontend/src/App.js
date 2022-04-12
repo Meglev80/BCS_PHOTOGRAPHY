4983127f0da59161aacd9e843c022e712d9ae5e7
Meglev80 <98914192+Meglev80@users.noreply.github.com>
2022-04-12 12:52:29 -0400

    BCS_PHOTOGRAPHY

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Brian Simms Photography</a>
+      </header>
+      <main>Product List</main>
+    </div>
+  );
+}
+
+export default App;
